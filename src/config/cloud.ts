cbde7b211170bed39d17ad7de6d43ddd72e3f547
WenxinFu050406 <2756470494@qq.com>
2026-05-27 19:10:27 +0800

    更新云开发环境

--- src/config/cloud.ts
+++ src/config/cloud.ts
@@ -1,3 +1,3 @@
-export const WECHAT_CLOUD_ENV_ID = 'cloudbase-1gejb315ceb7467b'
+export const WECHAT_CLOUD_ENV_ID = 'koko-d4gby9etz4b8f70f3'
 
 export const isWechatCloudConfigured = () => WECHAT_CLOUD_ENV_ID.trim().length > 0
